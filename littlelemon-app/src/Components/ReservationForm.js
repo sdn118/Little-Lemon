aa1d7bd9c6cf00d2d10312e68233e75e29b87a1b
Sheree Nicholson <sdnicholson8@gmail.com>
2023-09-15 17:06:54 -0400

    re-writing reservation form

--- littlelemon-app/src/Components/ReservationForm.js
+++ littlelemon-app/src/Components/ReservationForm.js
@@ -8,191 +8,169 @@ const occasions=[
   {id:"4", name:"Anniversary"},
 ]
 
-function ReservationForm(props){
+function Reservationpage (props) {
 
+  const [inputFields, setInputFields] = useState({
+    fname:"",
+    email:"",
+    phone:"",
+    date: "",
+    time: "",
+    guest:"",
+    occasion: ""
+  });
 
+  const [errors, setErrors] = useState({});
+  const [submit, setSubmit] = useState(false);
+  const [date, setDate] = useState({});
 
-const [fName, setFName] = useState("")
-const [lName, setLName] = useState("")
-const [email, setEmail] = useState("")
-const [date, setDate] = useState("");
-const [guest, setGuest] = useState("");
-const [occasion, setOccasion] = useState("");
-const [error, setError] = useState("");
+  const [occasions, setOccasions] = useState({});
 
-useEffect(()=>{
-  setOccasion(occasions);
-}, []);
+  const [selectedTime, setSelectedTime] = useState(
+    props.availableTimes.map((time) => <option>{time}</option>)
+  )
 
-const [selectedTime, setSelectedTime] = useState(
-  props.availableTimes.map((times) => <option>{times}</option>)
-)
+  function handleDateChange(e) {
+    setDate(e.target.value);
 
-function handleDateChange(e) {
-  setDate(e.target.value);
+    var stringify = e.target.value;
+    const date = new Date(stringify);
 
-  var stringify = e.target.value;
-  const date = new Date(stringify);
+    props.updateTimes(date);
 
-  props.updateTimes(date);
-
-  setSelectedTime(props.availableTimes.map((times) => <option>{times}</option>));
-}
-
-const validateForm = () => {
-  let error = ""
-
-  if (fName === "") {
-    error.fName = "First Name required."
+    setSelectedTime(props.availableTimes.map((time) => <option>{time}</option>));
   }
 
-  if (lName === "") {
-    error.lName = "Last Name required."
+  const validateValues = (inputFields) => {
+    let errors = {};
+    if (inputFields.fname.length < 25) {
+      errors.fname = "Full name too short.";
+    }
+    if (inputFields.email.length < 5){
+      errors.email = "Email is required"
+    }
+    if (inputFields.phone.length <= 9){
+      errors.phone = "Phone number too short"
+    } else if (inputFields.phone.length >= 11){
+      errors.phone = "Phone number too long"
+    }
+    if (inputFields.date = {}){
+      errors.date = "Please select a date"
+    }
+    if (inputFields.time = {}){
+      errors.time = "Please select a time"
+    }
+    if (inputFields.guest = {}){
+      errors.guest = "Please select a number of guest"
+    }
+    if (inputFields.occasion = {}){
+      errors.occasion = "Occasion not selected"
+    }
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    setInputFields({...inputFields, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateValues(inputFields));
+    setSubmit(true);
   }
 
-  if (email === "") {
-    error.email = "Email required."
-  } else if (email === "/^+([-]?+)*@+([-]?+)*({2,3})+$/") {
-    error.email = "Email invalid"
-  }
+  const finishSubmit = () => {
+    console.log(inputFields);
+  };
+  useEffect(() => {
+    if (Object.keys(errors).length ===0 && submit) {
+      finishSubmit();
+    }
+  }, [errors]);
 
-  if (date === "") {
-    error.date= "Date required."
-  }
-
-  if (guest === "") {
-    error.guest = "Please select number of guest."
-  }
-
-  setError("...error")
-  return Object.keys(error).length <1
-}
+  useEffect(()=>{
+    setOccasions(occasions);
+  }, []);
 
 
+return(
+<div>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="fname">Full Name</label>
+    <input type="text"
+        id="fname"
+       name="fname"
+       placeholder="Full Name"
+       value={inputFields.fname}
+       onChange={handleChange}/>
+       <span className="non-valid">{errors.fName}</span>
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-
-  let isValid = validateForm()
-  if(isValid) {
-    console.log("Submitted")
-  }
-}
-
-const navigate = useNavigate();
-
-
-
-
-
-
-  return(
-  <>
-    <h1 className="main">Book a Table!</h1>
-    <div className="reserve-wrapper">
-      <form
-      id="reservation-form"
-      className="reservation"
-      onSubmit={handleSubmit}>
-
-        <label htmlFor="fName">First Name</label>
-        <input type="text"
-        id="fName"
-        name="fName"
-        required value={fName}
-        placeholder="First Name"
-        onChange={(e) => setFName(e.target.value)}
-        />
-        <span className="non-valid">{error.fName}</span>
-
-
-        <label htmlFor="lName">Last Name</label>
-        <input type="text"
-        id="lName"
-        name="lName"
-        required value={lName}
-        placeholder="Last Name"
-        onChange={(e) => setLName(e.target.value)}
-        />
-        <span className="non-valid">{error.lName}</span>
-
-
-        <label htmlFor="email">Email</label>
-        <input type="email"
+    <label htmlFor="email">Email</label>
+    <input  type="text"
         id="email"
         name="email"
-        required value={email}
         placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-        />
-        <span className="non-valid">{error.email}</span>
+        value={inputFields.email}
+        onChange={handleChange}/>
+        <span className="non-valid">{errors.email}</span>
 
-          <label htmlFor="date">Choose a Date</label>
+    <label htmlFor="phone">Phone</label>
+    <input type="text"
+        id="phone"
+        name="phone"
+        placeholder="Phone Number"
+        value={inputFields.phone}
+        onChange={handleChange}/>
+        <span className="non-valid">{errors.phone}</span>
 
-          <input
-          type="date"
-          id="date"
-          name="date"
-          required value={date}
-          onChange={handleDateChange}
-          />
-          <span className="non-valid">{error.date}</span>
+    <label htmlFor="date">date</label>
+    <input type="date"
+        id="date"
+        name="date"
+        value={date}
+        onChange={handleDateChange}/>
+        <span className="non-valid">{errors.date}</span>
 
-          <label htmlFor="time">Choose a Time</label>
-          <select id="time" required>
-            {selectedTime}
-          </select>
+    <label htmlFor="time">Time</label>
+    <input type="time"
+        id="time"
+        name="time"
+        value={selectedTime}
+        onChange={handleChange}/>
+        <span className="non-valid">{errors.time}</span>
 
-          <label htmlFor="guest">Number of Guests</label>
+    <label htmlFor="guest">Guest</label>
+    <input type="number"
+        id="guest"
+        name="guest"
+        placeholder="1"
+        value={inputFields.guest}
+        onChange={handleChange}/>
+        <span className="non-valid">{errors.guest}</span>
 
-          <input
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          name="guest"
-          id="guest"
-          required value={guest}
-          onChange={(e) => setGuest(e.target.value)}
-          />
-          <span className="non-valid">{error.guest}</span>
-
-
-
-        <div className="dropdown">
-
-        <select
-        id="dropdown-control"
-        name="dropdown-control"
-        type="dropdown-control"
-        required value= {occasion}
-        htmlFor="dropdown-control"
-        onChange={(e) => setOccasion(e.target.value)}
-        >
-          <option value="0">Select Occasion</option>
-        {
-            occasions &&
+      <label htmlFor="occasion">Occasion</label>
+      <select type="button"
+        id="occasion"
+        name="occasion"
+        value={inputFields.occasion}
+        onChange={handleChange}>
+        <option value="0">Select Occasion</option>
+          {occasions &&
             occasions !== undefined ?
-            occasions.map((ocs, index)=>{
-              return(
-                <option key={index} value={ocs.id}>{ocs.name}</option>
-              )
-            })
-            : "Occasion not selected"
-          }
+            occasions.map((ocs, index) => {
+            return (
+              <option key={index} value={ocs.id}>{ocs.name}</option>
+            )
+          })
+          : "Occasion not selected"
+        }
+        </select>
 
-          </select>
-        </div>
+    <input type="submit" name="submit" id="submit"/>
+  </form>
+</div>
 
-        <div>
-          <input type="submit" id="submit" name="submit" onClick={() => navigate ("/ConfirmedReservation")}/>
-        </div>
+);
+}
 
-      </form>
-    </div>
-  </>
-
-  );
-
-  }
-export default ReservationForm;
+export default Reservationpage
